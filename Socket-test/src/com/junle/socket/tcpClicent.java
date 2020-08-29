b6229c3e56c39da19652bf64973065c4fd7a571f
BabyYoung <1515251178@qq.com>
2020-08-29 17:40:29 +0800

    socket练习

---
+++ Socket-test/src/com/junle/socket/tcpClicent.java
@@ -0,0 +1,39 @@
+package com.junle.socket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class tcpClicent {
+    public static void main(String[] args) {
+        Socket socket=null;
+        OutputStream os=null;
+        try {
+            InetAddress serverip=InetAddress.getByName("127.0.0.1");
+            int port= 9999;
+            //创建连接
+            socket= new Socket(serverip,port);
+            //发送IO流
+            os=socket.getOutputStream();
+            os.write("hello，你好呀!".getBytes());
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (os!=null){
+                try {
+                    os.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if (socket!=null){
+                try {
+                    socket.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ Socket-test/src/com/junle/socket/tcpServer.java
@@ -0,0 +1,70 @@
+package com.junle.socket;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class tcpServer {
+    public static void main(String[] args) {
+        ServerSocket serverSocket=null;
+        Socket socket=null;
+        InputStream ins=null;
+        ByteArrayOutputStream baos=null;
+
+
+        try {
+            serverSocket= new ServerSocket(9999);
+            while (true){
+
+                socket=serverSocket.accept();
+                //读取客户端的消息
+                ins=socket.getInputStream();
+                baos=new ByteArrayOutputStream();
+                byte[] buffer=new byte[1024];
+                int lenth;
+                while ((lenth=ins.read(buffer))!=-1){
+                    baos.write(buffer,0,lenth);
+                }
+                System.out.println(baos.toString());
+        }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            //关闭资源
+            if (baos!=null){
+                try {
+                    baos.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if (ins!=null){
+                try {
+                ins.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if (socket!=null){
+                try {
+                socket.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if (serverSocket!=null){
+                try {
+                serverSocket.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
+    }
+}
