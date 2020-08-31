f86c534e1c35613205843a89ac923862e79e53b3
BabyYoung <1515251178@qq.com>
2020-09-01 01:22:07 +0800

    map

--- HashMap-ConcurrentHashMap/src/com/junle/ConcurrentHashMap.java
+++ HashMap-ConcurrentHashMap/src/com/junle/ConcurrentHashMap.java
@@ -9,19 +9,21 @@ class ConcurrentHashmap {
             @Override
             public void run() {
                 Map<Long, String> conMap = new ConcurrentHashMap<>();
-                for (long i = 0; i < 1000; i++) {
+                for (long i = 0; i < 10000; i++) {
                     conMap.put(i, "");
                 }
+                //concurrenthashmap可同时进行读和删除
                 for (Map.Entry<Long, String> entry : conMap.entrySet()) {
                     Long key = entry.getKey();
                     if (key%2==0) {
                         conMap.remove(key);
                     }
                 }
-//concurrenthashmap可同时进行读写
+              //concurrenthashmap可同时进行读写
                 for (Map.Entry<Long, String> entry : conMap.entrySet()) {
                     System.out.println(entry.getKey() + " " + entry.getValue());
                 }
+                System.out.println("数量为"+conMap.size());
             }
         });
         thread.start();
--- HashMap-ConcurrentHashMap/src/com/junle/Hashmap.java
+++ HashMap-ConcurrentHashMap/src/com/junle/Hashmap.java
@@ -6,17 +6,20 @@ import java.util.UUID;
 public class Hashmap {
 
     public static void main(String[] args) {
-        final HashMap<String, String> map = new HashMap<String, String>(2);
-        for (int i = 0; i < 100000; i++) {
+        final HashMap<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i <=10000; i++) {
             int finalI = i;
             new Thread(new Runnable() {
                 @Override
-                public void run() {
-                    map.put(UUID.randomUUID().toString(), "");
-                    System.out.println(map);
+                public  void  run() {
+                    synchronized (map) {
+                        map.put(UUID.randomUUID().toString(), "");
+                    }
                 }
+
             }).start();
         }
+        System.out.println(map.size());
     }
 
 }
